79a47e9d6f1ddbc0c01df9bf3ba9463fbcea6b7b
1337isnot1337 <cinnajdavis@proton.me>
2024-08-21 23:28:53 -0500

    more rust

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 use actix_files::Files;
-use actix_multipart::Multipart;
-use actix_web::{web, HttpResponse, Responder, Result};
-use futures::StreamExt as _;
-use futures::TryStreamExt;
+use actix_web::web;
 use serde::{Deserialize, Serialize};
 use serde_json::from_reader;
 use std::path::Path;
-use std::path::PathBuf;
-use tokio::io::AsyncWriteExt;
+use webpages::{css_handler, directions, editor, home_page, image, input, save, schedule_handle};
+mod path;
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 struct Node {
@@ -103,77 +100,100 @@ mod pathfinding {
     }
 }
 
-async fn home_page() -> impl Responder {
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body(std::fs::read_to_string("assets/home.html").unwrap())
-}
+mod webpages {
+    use crate::{
+        file_utils, name_to_id, path::get_schedule, path::path, pathfinding, reset_nodes, Node,
+    };
+    use actix_multipart::Multipart;
+    use actix_web::{web, HttpResponse, Responder, Result};
+    use futures::StreamExt as _;
+    use futures::TryStreamExt;
+    use tokio::io::AsyncWriteExt;
 
-async fn editor() -> impl Responder {
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body(std::fs::read_to_string("assets/editor.html").unwrap())
-}
+    pub async fn home_page() -> impl Responder {
+        HttpResponse::Ok()
+            .content_type("text/html")
+            .body(std::fs::read_to_string("assets/home.html").unwrap())
+    }
+    pub async fn input() -> impl Responder {
+        HttpResponse::Ok()
+            .content_type("text/html")
+            .body(std::fs::read_to_string("assets/input/index.html").unwrap())
+    }
 
-async fn image() -> impl Responder {
-    HttpResponse::Ok()
-        .content_type("image/jpeg")
-        .body(std::fs::read("assets/imsa_hallway.jpg").unwrap())
-}
+    pub async fn schedule_handle(
+        web::Json(request): web::Json<serde_json::Value>,
+    ) -> impl Responder {
+        let user_input = request["Schedule Input"].as_str().unwrap();
+        let schedule = path(&get_schedule(user_input).unwrap());
+        HttpResponse::Ok().json(serde_json::json!({ "Schedule Input": schedule }))
+    }
 
-async fn css_handler() -> impl Responder {
-    let css = tokio::fs::read_to_string("assets/home.css").await.unwrap();
-    HttpResponse::Ok().content_type("text/css").body(css)
-}
+    pub async fn editor() -> impl Responder {
+        HttpResponse::Ok()
+            .content_type("text/html")
+            .body(std::fs::read_to_string("assets/editor.html").unwrap())
+    }
 
-async fn save(mut payload: Multipart) -> Result<HttpResponse> {
-    while let Ok(Some(mut field)) = payload.try_next().await {
-        if let Some("file") = field.name() {
-            let data = field.next().await.unwrap().unwrap();
-            let mut file = tokio::fs::File::create("nodes.json").await.unwrap();
-            file.write_all(&data).await.unwrap();
+    pub async fn image() -> impl Responder {
+        HttpResponse::Ok()
+            .content_type("image/jpeg")
+            .body(std::fs::read("assets/imsa_hallway.jpg").unwrap())
+    }
+
+    pub async fn css_handler() -> impl Responder {
+        let css = tokio::fs::read_to_string("assets/home.css").await.unwrap();
+        HttpResponse::Ok().content_type("text/css").body(css)
+    }
+
+    pub async fn save(mut payload: Multipart) -> Result<HttpResponse> {
+        while let Ok(Some(mut field)) = payload.try_next().await {
+            if let Some("file") = field.name() {
+                let data = field.next().await.unwrap().unwrap();
+                let mut file = tokio::fs::File::create("nodes.json").await.unwrap();
+                file.write_all(&data).await.unwrap();
+            }
         }
-    }
-    Ok(HttpResponse::Ok().json(serde_json::json!({"status": 0})))
-}
-
-async fn directions(web::Json(request): web::Json<serde_json::Value>) -> impl Responder {
-    let mut nodes: Vec<Node> =
-        file_utils::read_nodes_from_file("assets/nodes.json").expect("Failed to read nodes");
-
-    reset_nodes(&mut nodes);
-
-    let start_room =
-        name_to_id(request["start-room"].as_str().unwrap(), &nodes).unwrap_or(usize::MAX);
-    let destination =
-        name_to_id(request["destination"].as_str().unwrap(), &nodes).unwrap_or(usize::MAX);
-
-    println!("Received request: start-room = {start_room}, destination = {destination}");
-
-    if start_room == usize::MAX || destination == usize::MAX {
-        println!("Error: Invalid start or destination room");
-        return HttpResponse::BadRequest().json(serde_json::json!({"status": 1}));
+        Ok(HttpResponse::Ok().json(serde_json::json!({"status": 0})))
     }
 
-    let shortest_path = pathfinding::time_path(start_room, destination, &mut nodes);
+    pub async fn directions(web::Json(request): web::Json<serde_json::Value>) -> impl Responder {
+        let mut nodes: Vec<Node> =
+            file_utils::read_nodes_from_file("assets/nodes.json").expect("Failed to read nodes");
 
-    let path_json: Vec<serde_json::Value> = shortest_path
-        .iter()
-        .map(|&index| {
-            serde_json::json!({
-                "x": nodes[index].x,
-                "y": nodes[index].y,
-                "name": nodes[index].name,
-                "index": index
+        reset_nodes(&mut nodes);
+
+        let start_room =
+            name_to_id(request["start-room"].as_str().unwrap(), &nodes).unwrap_or(usize::MAX);
+        let destination =
+            name_to_id(request["destination"].as_str().unwrap(), &nodes).unwrap_or(usize::MAX);
+
+        println!("Received request: start-room = {start_room}, destination = {destination}");
+
+        if start_room == usize::MAX || destination == usize::MAX {
+            println!("Error: Invalid start or destination room");
+            return HttpResponse::BadRequest().json(serde_json::json!({"status": 1}));
+        }
+
+        let shortest_path = pathfinding::time_path(start_room, destination, &mut nodes);
+
+        let path_json: Vec<serde_json::Value> = shortest_path
+            .iter()
+            .map(|&index| {
+                serde_json::json!({
+                    "x": nodes[index].x,
+                    "y": nodes[index].y,
+                    "name": nodes[index].name,
+                    "index": index
+                })
             })
-        })
-        .collect();
+            .collect();
 
-    println!("Returning path JSON: {path_json:?}");
+        println!("Returning path JSON: {path_json:?}");
 
-    HttpResponse::Ok().json(serde_json::json!({ "path": path_json }))
+        HttpResponse::Ok().json(serde_json::json!({ "path": path_json }))
+    }
 }
-
 fn reset_nodes(nodes: &mut [Node]) {
     for node in nodes.iter_mut() {
         node.dist = f64::INFINITY;
@@ -201,16 +221,11 @@ async fn shuttle_main(
                 .route("/home.css", web::get().to(css_handler))
                 .route("/save", web::post().to(save))
                 .route("/get_directions", web::post().to(directions))
+                .route("/schedule-post", web::post().to(schedule_handle))
                 .route("/input", web::get().to(input))
-                
                 .service(Files::new("/assets", "assets")),
         );
     };
 
     Ok(shuttle_actix_web::ActixWebService(factory))
-}
-async fn input() -> impl Responder {
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body(std::fs::read_to_string("assets/input/index.html").unwrap())
 }
